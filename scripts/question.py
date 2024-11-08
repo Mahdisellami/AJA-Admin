0c1f8875ac7cc7cf6df004c5c7ef004c7f55beea
Hani Gadhoumi <gadhoumi@fortiss.org>
2024-11-08 21:15:32 +0100

    version2

--- scripts/question.py
+++ scripts/question.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import json
 import requests
@@ -28,34 +27,3 @@ question_data = {
 
 
 post_to_api(question_data)
-=======
-import pandas as pd
-import json
-import requests
-
-file_path = 'Allegations.csv'  
-data = pd.read_csv(file_path, delimiter=';', quotechar='"')
-data = data.fillna('')
-data.columns = data.columns.str.strip()
-unique_allegations = data['allegation'].unique()
-def post_to_api(json_data):
-    url = 'http://localhost:8000/api/questions'  
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, data=json.dumps(json_data), headers=headers)
-    if response.status_code == 201:
-        print("Question successfully posted.")
-    else:
-        print(f"Failed to post question. Status code: {response.status_code}, Response: {response.text}")
-
-answers = [{"answer": allegation, "nextQuestionId": None} for allegation in unique_allegations]
-question_data = {
-    "text": "text",  
-    "theme": "energy", 
-    "isThemeRelated": True, 
-    "multiple": False,  
-    "answers": answers  
-}
-
-
-post_to_api(question_data)
->>>>>>> acc01cee3d75a35c9edb9102300b8c3179fc5624
