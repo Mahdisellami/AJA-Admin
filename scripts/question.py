c6b67922cab6dc3feaddecc9fe746e441e6c3d34
Hani Gadhoumi <gadhoumi@fortiss.org>
2024-11-08 21:17:18 +0100

    version2

--- scripts/question.py
+++ scripts/question.py
@@ -2,28 +2,31 @@ import pandas as pd
 import json
 import requests
 
-file_path = 'Allegations.csv'  
-data = pd.read_csv(file_path, delimiter=';', quotechar='"')
+# Load the CSV file and process it
+file_path = 'alleg.csv'  
+data = pd.read_csv(file_path, delimiter=';', quotechar='"', encoding='ISO-8859-1')
 data = data.fillna('')
 data.columns = data.columns.str.strip()
-unique_allegations = data['allegation'].unique()
+
+# Function to post data to the API
 def post_to_api(json_data):
-    url = 'http://api:8000/api/questions'  
+    url = 'http://localhost:8000/api/questions'
     headers = {'Content-Type': 'application/json'}
     response = requests.post(url, data=json.dumps(json_data), headers=headers)
     if response.status_code == 201:
         print("Question successfully posted.")
     else:
         print(f"Failed to post question. Status code: {response.status_code}, Response: {response.text}")
-
-answers = [{"answer": allegation, "nextQuestionId": None} for allegation in unique_allegations]
-question_data = {
-    "text": "text",  
-    "theme": "energy", 
-    "isThemeRelated": True, 
-    "multiple": False,  
-    "answers": answers  
-}
-
-
-post_to_api(question_data)
+for theme in data['theme'].unique():
+    theme_data = data[data['theme'] == theme]
+    answers = [{"answer": allegation, "nextQuestionId": None} for allegation in theme_data['allegation'].unique()]
+    question_data = {
+        "text": "text",  
+        "theme": theme,  
+        "isThemeRelated": True, 
+        "multiple": False,  
+        "answers": answers  
+    }
+    
+    # Post question data to the API
+    post_to_api(question_data)
